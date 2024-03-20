782c7c89d461d70dd3d0ef5f70d45ce83b482596
Marco André <marcoandre2103@gmail.com>
2024-03-20 15:22:18 +0000

    Nothing

--- LightGame/Assets/Scripts/Entities/Attacks/MageSpell.cs
+++ LightGame/Assets/Scripts/Entities/Attacks/MageSpell.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class MageSpell : ProtoAttack
 {
     public override void Fire(int damage = 10, Vector3 direction = default(Vector3), float speed = 200.0f, float gravity = 0.0f, float despawnTime = 7.5f){
-        base.Fire(damage, direction, speed: 0.0f, despawnTime: 0.2f);
+        base.Fire(damage, direction, speed: 50f, despawnTime: 10f);
     }
 }
