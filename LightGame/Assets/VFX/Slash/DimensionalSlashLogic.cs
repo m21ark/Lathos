90f3dba86abbb7ec8023e3b62b0853af842f4ca3
Johnny-droid <joaoricardoralves@gmail.com>
2024-05-07 16:46:01 +0100

    Added dimensional slash (special knight)

--- LightGame/Assets/VFX/Slash/DimensionalSlashLogic.cs
+++ LightGame/Assets/VFX/Slash/DimensionalSlashLogic.cs
@@ -8,35 +8,18 @@ public class DimensionalSlashScreenLogic : MonoBehaviour
     [SerializeField] private float delay;
     [SerializeField] private float slashDuration;
     [SerializeField] private ScriptableRendererFeature fullScreenInverted;
-    [SerializeField] private Material _material;
-    [SerializeField] private AnimationCurve offesetCurve;
-    [SerializeField] private bool isHorizontal;
-    private float _timer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _timer = 0.0f;
-        _material.SetFloat("_Offset", 0.0f);
-        _material.SetFloat("_IsHorizontal", isHorizontal ? 1.0f : 0.0f);
+
+    void Start() {
+        // Start the coroutine to change the view to inverted colors
+        StartCoroutine(ChangeColors());
+    }
+
+    IEnumerator ChangeColors() {
+        yield return new WaitForSeconds(delay);
         fullScreenInverted.SetActive(true);
+        yield return new WaitForSeconds(slashDuration);
+        fullScreenInverted.SetActive(false);
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_timer < delay) {
-            _timer += Time.deltaTime;
-            return;
-        }
-
-        // Update the material according to the fade curve
-        float offset = offesetCurve.Evaluate((_timer-delay) / slashDuration);
-        _material.SetFloat("_Offset", offset);
-        _timer += Time.deltaTime;
-
-        if (_timer - delay >= slashDuration) {
-            fullScreenInverted.SetActive(false);
-            Destroy(gameObject);
-        }
-    }
 }
