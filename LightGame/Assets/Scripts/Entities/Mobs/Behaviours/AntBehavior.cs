00d4099548b72e97c88f37c89c77f4a9df0e2c4d
Marco André <marcoandre2103@gmail.com>
2024-05-12 11:34:09 +0100

    Ant car movement test 1

--- LightGame/Assets/Scripts/Entities/Mobs/Behaviours/AntBehavior.cs
+++ LightGame/Assets/Scripts/Entities/Mobs/Behaviours/AntBehavior.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class AntBehavior : ProtoMob
 {
-    GameObject player;
+    GameObject player = null;
 
     void Update()
     {
         Move();
-        if (GameLogic.instance.player)
-            player = GameLogic.instance.player.gameObject;
     }
 
     public override void Move()
     {
-
         if (player != null)
         {
             // Calculate direction vector towards the player
             Vector3 direction = (player.transform.position - transform.position).normalized;
             direction.y = 0f; // Ensure the minion doesn't move up or down
-
-            // Normalize the direction vector
             direction.Normalize();
 
             // Move the minion towards the player
@@ -30,33 +25,35 @@ public class AntBehavior : ProtoMob
 
             // Get the child object that contains the mesh
             GameObject mesh = transform.GetChild(0).gameObject;
-            mesh.transform.rotation = Quaternion.Slerp(mesh.transform.rotation, Quaternion.LookRotation(-direction), 0.1f);
 
-            // TODO: make head turn correctly and make the ant like a car axis with a head that turns
-            
+            // Calculate rotation based on movement direction
+            Quaternion targetRotation = Quaternion.LookRotation(-direction);
+
+            // Apply rotation smoothly
+            mesh.transform.rotation = Quaternion.Slerp(mesh.transform.rotation, targetRotation, 0.1f);
+
+            // Calculate torque axis based on movement direction
+            Vector3 torqueAxis = Vector3.Cross(mesh.transform.forward, direction);
+
+            // Apply torque to simulate car wheel axis
+            Rigidbody rb = GetComponent<Rigidbody>();
+            float torqueAmount = 3.0f;
+            rb.AddTorque(torqueAxis * torqueAmount, ForceMode.Impulse);
         }
+        else if (GameLogic.instance.player)
+            player = GameLogic.instance.player.gameObject;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-
         // Deal Damage
         if (collision.gameObject.CompareTag("Player"))
-        {
             GameLogic.instance.player.TakeDamage(damage);
-            JumpAwayFromPlayer();
-        }
-
+        
     }
 
-    void JumpAwayFromPlayer()
+    void OnCollisionStay(Collision other)
     {
-        // Calculate direction away from the player
-        Vector3 direction = transform.position - player.transform.position;
-        direction.y = 0f;
-        direction.Normalize();
-
-        float jumpForce = 20.0f;
-        GetComponent<Rigidbody>().AddForce(direction * jumpForce, ForceMode.Impulse);
+        OnCollisionEnter(other);
     }
 }
